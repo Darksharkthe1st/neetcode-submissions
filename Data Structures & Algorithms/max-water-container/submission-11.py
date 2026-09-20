3403b35e3d0ce0a39f961f7ad2f40b48b6efc17f
Farhan Kittur <kitturfarhan@gmail.com>
2026-09-20 16:36:58 -0400

    Add: max-water-container - submission-11

---
+++ Data Structures & Algorithms/max-water-container/submission-11.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # BRUTE FORCE SOLN
+
+        # maxArea = 0
+        # for i in range(len(heights)):
+        #     for j in range(i + 1, len(heights)):
+        #         area = min(heights[i], heights[j]) * (j - i)
+        #         if (area > maxArea):
+        #             maxArea = area
+        
+        # return maxArea
+
+        i = 0
+        j = len(heights) - 1
+        maxArea = 0
+        while i < j:
+            # print(i, j)
+            maxArea = max(min(heights[i], heights[j]) * (j-i), maxArea)
+            # j_loss = heights[j] - heights[j-1]
+            # i_loss = heights[i] - heights[i+1]
+            # if j_loss == i_loss:
+            #     if heights[j] < heights[i]:
+            #         j -= 1
+            #     else:
+            #         i += 1
+            # elif j_loss > i_loss:
+            #     i += 1
+            # else:
+            #     j -= 1
+
+            if heights[j] < heights[i]:
+                j -= 1
+            else:
+                i += 1
+        
+
+
+        return maxArea
+
+
